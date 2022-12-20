157f8685ea3e529f739f3c0d6fd051382b99c466
Antonio-Italiano <antoitalia221@gmail.com>
2022-12-20 16:28:10 +0100

    Add take Dom Element

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('js ok')
+// ELEMENTI DOM 
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+// console.log(days, hours, minutes, seconds);
+
+
